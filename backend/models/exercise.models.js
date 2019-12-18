5915e0a7abdf9fd3470f928048825f7a334522c0
Hong Yi Chen <hongyigma@gmail.com>
2019-12-17 23:51:45 -0500

    Added models for users and exercises

---
+++ backend/models/exercise.models.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+// 4 fields, not as many validations
+const exerciseSchema = new Schema({
+    username: { type: String, required: true},
+    description: { type: String, required: true},
+    duration: { type: Number, required: true},
+    date: { type: Date, required: true},
+}, {
+    timestamps: true,
+});
+
+const Exercise = mongoose.model('Exercise', exerciseSchema);
+
+module.exports = Exercise;
---
+++ backend/models/user.model.js
@@ -0,0 +1,25 @@
+import { Schema } from "mongoose";
+
+const mongoose = require('mongoose');
+// All mongoose Schemas basically start the same
+const Schema = mongoose.Schema;
+
+// this only has a single field (username)
+// below are validations, e.g, trim will trim whitespace off the end
+const userSchema = new Schema({
+    username: {
+        type: String,
+        required: true,
+        unique: true,
+        trim: true,
+        minlength: 3
+    }
+}, {
+    timestamps: true, // creates fields for creation/modification
+});
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
+
+// many of this will look very similar for mongoose schemas
