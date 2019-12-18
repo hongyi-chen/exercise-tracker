8c2e77097ae87084c8cb33fdc595a1c26e3b7793
Hong Yi Chen <hongyigma@gmail.com>
2019-12-18 11:42:58 -0500

    Set up CRUD routing

--- backend/routes/exercises.js
+++ backend/routes/exercises.js
@@ -31,6 +31,40 @@ router.route('/add').post((req, res) => {
     .catch(err => res.status(400).json('Error: ' + err));
 });
 
+// /:id is like a variable, an object id that is created automatically my Mongo
+// this returns just the information about a specific exercise (JSON)
+// the get request
+router.route('/:id').get((reqz, res) => {
+    Exercise.findById(req.params.id)
+    .then(exercise => res.json(exercise))
+    .catch(err => res.status(400).json('Error: ' + err));
+});
 
+// this is the delete request, deletes the object based on its ID
+// finds the ID and then deletes (gets it from the url)
+router.route('/:id').delete((req, res) => {
+    Exercise.findByIdAndDelete(req.params.id) //req.params.id is the URL
+    .then(() => res.json('Exercise deleted.'))
+    .catch(err => res.status(400).json('Error: ' + err));
+});
+
+// finds the current exercise, passing in the parameter from the URL
+// take the information from the exercise, you set it to the new parameters
+// assigning it to the fields of the exercises that already exist
+// save it, and then return
+router.route('/update/:id').post((req, res) => {
+    Exercise.findById(req.params.id)
+    .then(exercise => {
+        exercise.username = req.body.username;
+        exercise.description = req.body.description;
+        exercise.duration = Number(req.body.duration);
+        exercise.date = Date.parse(req.body.date);
+
+        exercise.save()
+        .then(() => res.json('Exercise updated!'))
+        .catch(err => res.status(400).json('Error: ' + err));
+    })
+    .catch(err => res.status(400).json('Error: ' + err))
+})
 
 module.exports = router;
